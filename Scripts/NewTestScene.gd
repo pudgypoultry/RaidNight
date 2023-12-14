9b7ad57a285250bf5ffbc3295772a0441828a14b
pudgypoultry <50252390+pudgypoultry@users.noreply.github.com>
2023-12-14 03:45:45 -0800

    Pushing work

--- Scripts/NewTestScene.gd
+++ Scripts/NewTestScene.gd
@@ -22,10 +22,18 @@ func handle_tile_clicked():
 	if Input.is_action_just_pressed("left_mouse_click"):
 
 		for tile in hexBoard.get_children():
-			if tile.mouse_in == true:
-				hexBoard.reset_board_color()
-				currentQ = tile.q
-				currentR = tile.r
-				currentS = tile.s
-				currentTile = hexBoard.get_tile(currentQ, currentR, currentS)
-				hexBoard.test_walking_range(currentTile.q, currentTile.r, currentTile.s, 4, true)
+			if tile is Hexagon:
+				if tile.mouse_in == true:
+					hexBoard.reset_board_color()
+					currentQ = tile.q
+					currentR = tile.r
+					currentS = tile.s
+					currentTile = hexBoard.get_tile(currentQ, currentR, currentS)
+					hexBoard.test_walking_range(currentTile.q, currentTile.r, currentTile.s, 4, true)
+
+func handle_character_clicked():
+	pass
+	# check to see if they're friendly or not
+	# if not, just display info
+	# if so, select character and show movement range
+		# use handle_tile_clicked to move character to spot if clicked tile is in movement range
